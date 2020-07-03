c776515fb96459b63ac04146e3c74d0b54635a0b
Steven Snyder <sts12589@gmail.com>
2020-07-03 12:13:13 -0400

    added text file with all pronunciations

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -1,3 +1,43 @@
-function compute(expression) {
-    // TODO - write method definition here
+function arraysEqual(a1,a2) {
+    
+
+    return JSON.stringify(a1)==JSON.stringify(a2);
+
+
+}
+
+class PronunciationTest {
+	_test(expected, wordString) {
+		let word = new Word(wordString);
+
+		// when
+		let actual = word.getSimilarWordsWithSubPhonemes();
+		
+		// then
+		let pass = arraysEqual(expected, actual);
+
+		console.log('input = ' + wordString);
+		console.log('expected = ' + expected);		
+		console.log('actual = ' + actual);
+		console.log('test passed = ' + pass);
+	}
+
+	test1() {
+		let expected = ['D','OW1','N','AH2','T'];
+		let input = 'donut';
+		_test(expected, input);
+	}
+
+	test2() {
+		let expected = ['P','R','OW1','G','R','AE2','M'];
+		let input = 'program';
+		_test(expected, input);
+	}
+
+
+	test3() {
+		let expected = ['AH0','K','AO1','R','D'];
+		let input = 'accord';
+		_test(expected, input);
+	}
 }
